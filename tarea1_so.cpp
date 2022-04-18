165394eb16b5c45cc155f9adc1d3075483d3b9ce
MuffinElZangano <34992541+MuffinElZangano@users.noreply.github.com>
2022-04-18 19:23:13 -0400

    String Command-rev

--- tarea1_so.cpp
+++ tarea1_so.cpp
@@ -1,5 +1,20 @@
 #include<stdio.h>
+#include<string.h>
 
+
+//Los tres comandos simulados: printf, touch, cat (Imprimir, Crear Archivo, Leer Archivo)
+String command(){
+	char comando[25];
+	scanf("%s",comando);
+	switch(comando){
+		case "print":
+			break;
+		case "touch":
+			break;
+		case "cat":
+			break;
+	}
+}
 
 void main(){
 	
